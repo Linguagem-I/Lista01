8854fa87ae657624c632284ad7cb6e6eef33eeaa
Guilherme <ggrigon@users.noreply.github.com>
2016-03-11 21:47:24 -0300

    Create 5.c

---
+++ Rigon/5.c
@@ -0,0 +1,15 @@
+5
+
+a) scanf("%d", &valor);
+
+b) printf("o produto de %d e %d eh %d\n", x, y, x*y);
+
+c) somaTotal = primeiroNumero + segundoNumero;
+
+d) scanf("%d", &umInteiro);
+
+e) printf("O resto de %d dividido por %d eh %d\n, x, y, x%y);
+
+f) printf("A soma eh %d", x+y);
+
+g) printf("o valor fornecido eh %d", valor);
