b5ca5e238c755e8c2d446afb4926a91e6a4b50f7
Lorenzo Wendt de Freitas <lorenzo.wendt@outlook.com>
2016-03-11 21:51:44 -0300

    Update 5.c

--- Rigon/5.c
+++ Rigon/5.c
@@ -1,15 +1,22 @@
 5
 
-a) scanf("%d", &valor);
+a) Errado = scanf("d", valor);
+   Certo = scanf("%d", &valor);
 
-b) printf("o produto de %d e %d eh %d\n", x, y, x*y);
+b) Errado = printf("o produto de %d e %d eh %d\n", x, y);
+   Certo = printf("o produto de %d e %d eh %d\n", x, y, x*y);
 
-c) somaTotal = primeiroNumero + segundoNumero;
+c) Errado = primeiroNumero + segundoNumero = somaTotal
+   Certo = somaTotal = primeiroNumero + segundoNumero;
 
-d) scanf("%d", &umInteiro);
+d) Errado = scanf("%d", umInteiro); 
+   Certo = scanf("%d", &umInteiro);
 
-e) printf("O resto de %d dividido por %d eh %d\n, x, y, x%y);
+e) Errado = printf("O resto de %d dividido por %d eh %d\n, x, y, x%y);
+   Certo = printf("O resto de %d dividido por %d eh\n", x, y, x%y);
 
-f) printf("A soma eh %d", x+y);
+f) Errado = printf("A soma eh %d", x+y);
+   Certo = print(“A soma eh %d”, x+y);
 
-g) printf("o valor fornecido eh %d", valor);
+g) Errado = printf("o valor fornecido eh %d", valor);
+   Certo = printf(“o valor fornecido eh %d”, &valor);
